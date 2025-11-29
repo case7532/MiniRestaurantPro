c94f8e0297783463a72a87494af62f356ca3a85c
HungTM <87566758+case7532@users.noreply.github.com>
2025-11-29 23:34:38 +0700

    remove test

--- __mocks__/react-native-safe-area-context.js
+++
@@ -1,8 +0,0 @@
-const React = require('react');
-
-module.exports = {
-  SafeAreaProvider: ({children}) => children,
-  SafeAreaView: ({children}) => children,
-  useSafeAreaInsets: () => ({top: 0, right: 0, bottom: 0, left: 0}),
-  useSafeAreaFrame: () => ({x: 0, y: 0, width: 390, height: 844}),
-};
